366fdf41935515b8201083dca8f0727aaf78c9dd
sanjay1151 <35390831+sanjay1151@users.noreply.github.com>
2018-03-05 22:09:36 +0530

    Update 17.c

--- 17.c
+++ 17.c
@@ -1,1 +1,40 @@
+#include<stdio.h>
+
+int power(int x,int y)
+{
+    int i, prd = 1;
+    for(i = 0; i < y;i++)
+    {
+        prd = prd * x;
+    }
+    return prd;
+}
+
+int main() 
+{
+    int count = 0, r, n, sum = 0, temp1, temp2;
+    scanf ("%d",&n);
+    temp1 = n;
+    temp2 = n;
+    while(temp1 > 0)
+    {
+        r = temp1 % 10;
+        temp1 = temp1 / 10;
+        count++;
+    }
+    while(temp2 > 0)
+    {
+        r = temp2 % 10;
+        sum = sum + power(r,count);
+        temp2 = temp2 / 10;
+    }
+    if(sum == n)
+    {
+        printf("yes");
+    }
+    else 
+    {
+        printf("No");
+    }
+}
 
